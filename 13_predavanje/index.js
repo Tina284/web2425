dd415423622b89970357a527d7871b333826da68
Tina284 <77157761+Tina284@users.noreply.github.com>
2024-12-05 19:31:22 +0100

    Add files via upload

---
+++ 13_predavanje/index.js
@@ -0,0 +1,24 @@
+console.log("VARIJABLE");
+
+//DEKLARIRANJE VARIJABLI
+var ime = 'Ana';
+console.log(ime)
+var prezime = "Anić"
+tipPr = typeof(prezime)
+console.log(tipPr)
+console.log(typeof(prezime))
+console.log(ime+' '+prezime)
+
+var prvi = 10;
+var drugi = 7;
+console.log(typeof(prvi))
+var zbroj = prvi+drugi;
+console.log("Zbroj brojeva prvi i drugi je ", prvi+drugi);
+console.log(prvi/drugi);
+var r = prvi % drugi
+console.log(r)
+console.log("Prvi != drugi : ", prvi != drugi)
+console.log(false || false || true);
+console.log("Vrijednost prvog ", prvi);
+prvi += 2;
+console.log("Vrijednost nakon dodavanja ", prvi);
